913fd38628bb073d323c9ef6ce6fcc20b6d6272d
franco stagno <franco.stagno@fizzmod.com>
2022-07-08 16:44:30 -0300

    initial config

---
+++ src/views/Home.js
@@ -0,0 +1,6 @@
+import React from "react";
+import MainLayout from "../components/MainLayout";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<MainLayout />);
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+	entry: {
+		home: path.join(__dirname, "src/views/Home.js"),
+	},
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	mode: "development",
+	module: {
+		rules: [
+			{
+				test: /\.?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"],
+					},
+				},
+			},
+			{
+				test: /\.(png|jp(e*)g|gif|svg)$/,
+				use: ["file-loader"],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			chunks: ["home"],
+			template: path.join(__dirname, "src/html/index.html"),
+			filename: "index.html",
+		}),
+		new CopyPlugin({
+			patterns: [{ from: path.resolve(__dirname, "public") }],
+		}),
+		new Dotenv({
+			systemvars: true,
+		}),
+	],
+};
